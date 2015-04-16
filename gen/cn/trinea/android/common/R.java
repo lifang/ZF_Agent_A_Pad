0bea0190a42095dcf042bfbb29be5fa3f89fce92
yewei <243877643@qq.com>
2015-04-16 09:22:31 +0800

    yewei

--- gen/cn/trinea/android/common/R.java
+++ gen/cn/trinea/android/common/R.java
@@ -27,20 +27,20 @@ public final class R {
 		public static final int drop_down_list_header_release_min_distance = 0x7f060002;
 	}
 	public static final class drawable {
-		public static final int drop_down_list_arrow = 0x7f020015;
+		public static final int drop_down_list_arrow = 0x7f020017;
 	}
 	public static final class id {
-		public static final int drop_down_list_footer_button = 0x7f0b008c;
-		public static final int drop_down_list_footer_progress_bar = 0x7f0b008b;
-		public static final int drop_down_list_header_default_text = 0x7f0b0090;
-		public static final int drop_down_list_header_default_text_layout = 0x7f0b008e;
-		public static final int drop_down_list_header_image = 0x7f0b008f;
-		public static final int drop_down_list_header_progress_bar = 0x7f0b008d;
-		public static final int drop_down_list_header_second_text = 0x7f0b0091;
+		public static final int drop_down_list_footer_button = 0x7f0b00a0;
+		public static final int drop_down_list_footer_progress_bar = 0x7f0b009f;
+		public static final int drop_down_list_header_default_text = 0x7f0b00a4;
+		public static final int drop_down_list_header_default_text_layout = 0x7f0b00a2;
+		public static final int drop_down_list_header_image = 0x7f0b00a3;
+		public static final int drop_down_list_header_progress_bar = 0x7f0b00a1;
+		public static final int drop_down_list_header_second_text = 0x7f0b00a5;
 	}
 	public static final class layout {
-		public static final int drop_down_list_footer = 0x7f030013;
-		public static final int drop_down_list_header = 0x7f030014;
+		public static final int drop_down_list_footer = 0x7f030017;
+		public static final int drop_down_list_header = 0x7f030018;
 	}
 	public static final class string {
 		public static final int drop_down_list_footer_default_text = 0x7f070004;
--- src/com/comdo/zf_agent_a_pad/util/Config.java
+++ src/com/comdo/zf_agent_a_pad/util/Config.java
@@ -26,11 +26,10 @@ public class Config {
 
 	public static final String POSLIST = PATHS + "good/list";
 
-	public static String getmes=PATHS+"message/receiver/getAll";
+	public static String getmes = PATHS + "message/receiver/getAll";
 	public static final String SHARED = "zfandroid";
 	public static String CITY = "上海";
 	public static boolean isFRIST = false;
-
 
 	static Gson gson = new Gson();
 	// Apply List
@@ -62,45 +61,57 @@ public class Config {
 	// Terminal detail
 	public static final String TERMINAL_DETAIL = PATHS
 			+ "/terminal/getApplyDetails";
-    // Get msglist
-	public static final String GET_MSGLIST = PATHS
-			+ "message/receiver/getAll";
-	
-	//售后记录---售后单记录---列表
+	// Get msglist
+	public static final String GET_MSGLIST = PATHS + "message/receiver/getAll";
+
+	// 售后记录---售后单记录---列表
 	public static final String AGENTS_SEARCH = PATHS + "cs/agents/search";
-	//售后记录---售后单记录---取消申请
-	public static final String AGENTS_CANCELAPPLY = PATHS + "cs/agents/cancelApply";
-	//售后记录--售后单记录--增加物流信息
+	// 售后记录---售后单记录---取消申请
+	public static final String AGENTS_CANCELAPPLY = PATHS
+			+ "cs/agents/cancelApply";
+	// 售后记录--售后单记录--增加物流信息
 	public static final String AGENTS_ADDMARK = PATHS + "cs/agents/addMark";
-	//售后记录---售后单记录---详情
+	// 售后记录---售后单记录---详情
 	public static final String AGENTS_GETBYID = PATHS + "cs/agents/getById";
-	//售后记录---注销记录---列表
+	// 售后记录---注销记录---列表
 	public static final String CANCELS_GETALL = PATHS + "cs/cancels/search";
-	//售后记录---注销记录---取消申请
-	public static final String CANCELS_CANCELAPPLY = PATHS + "cs/cancels/cancelApply";
-	//售后记录---注销记录---重新提交注销
-	public static final String CANCELS_RESUBMITCANCEL = PATHS + "cs/cancels/resubmitCancel";
-	//售后记录---注销记录---详情
-	public static final String CANCELS_GETCANCELBYID = PATHS + "cs/cancels/getCanCelById";
-	//售后记录---更新资料记录---列表
+	// 售后记录---注销记录---取消申请
+	public static final String CANCELS_CANCELAPPLY = PATHS
+			+ "cs/cancels/cancelApply";
+	// 售后记录---注销记录---重新提交注销
+	public static final String CANCELS_RESUBMITCANCEL = PATHS
+			+ "cs/cancels/resubmitCancel";
+	// 售后记录---注销记录---详情
+	public static final String CANCELS_GETCANCELBYID = PATHS
+			+ "cs/cancels/getCanCelById";
+	// 售后记录---更新资料记录---列表
 	public static final String INFO_GETALL = PATHS + "update/info/search";
-	//售后记录---更新资料记录---取消申请
-	public static final String INFO_CANCELAPPLY = PATHS + "update/info/cancelApply";
-	//售后记录---更新资料记录---详情
-	public static final String INFO_GETINFOBYID = PATHS + "update/info/getInfoById";
-	//用户管理--获得该代理商下面所有用户
+	// 售后记录---更新资料记录---取消申请
+	public static final String INFO_CANCELAPPLY = PATHS
+			+ "update/info/cancelApply";
+	// 售后记录---更新资料记录---详情
+	public static final String INFO_GETINFOBYID = PATHS
+			+ "update/info/getInfoById";
+	// 用户管理--获得该代理商下面所有用户
 	public static final String USER_GETUSER = PATHS + "user/getUser";
-	//用户管理--删除用户
-	public static final String USER_DELECTAGENTUSER = PATHS + "user/delectAgentUser";
-	//用户管理--获得该代理商下面某个用户的相关终端列表
+	// 用户管理--删除用户
+	public static final String USER_DELECTAGENTUSER = PATHS
+			+ "user/delectAgentUser";
+	// 用户管理--获得该代理商下面某个用户的相关终端列表
 	public static final String USER_GETTERMINALS = PATHS + "user/getTerminals";
-	
+
+	// get code
+	public static final String GETCODE4PHONE = PATHS
+			+ "/user/sendPhoneVerificationCodeReg";
+
 	public static boolean CheckIsLogin(Context c) {
 		return true;
 
 	}
-	public static void PostSearch(Context context, int agentId,int type,String keys, int city_id,
-			int rows, int page, int orderType, HttpCallback callback) {
+
+	public static void PostSearch(Context context, int agentId, int type,
+			String keys, int city_id, int rows, int page, int orderType,
+			HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("agentId", agentId);
 		params.put("keys", keys);
@@ -133,6 +144,7 @@ public class Config {
 		new HttpRequest(context, callback).post(Config.POSLIST, params);
 		System.out.println("参数--" + params.toString());
 	}
+
 	public static void getApplyList(Context context, int customerId, int page,
 			int rows, HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -193,8 +205,9 @@ public class Config {
 		params.put("customerId", customerId);
 		new HttpRequest(context, callback).post(TERMINAL_DETAIL, params);
 	}
-	public static void getMsgList(Context context,int customerId,
-			int page,int rows,HttpCallback callback){
+
+	public static void getMsgList(Context context, int customerId, int page,
+			int rows, HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("customerId", customerId);
 		params.put("page", page);
@@ -203,16 +216,9 @@ public class Config {
 		Log.e("params", String.valueOf(params));
 		Log.e("url", GET_MSGLIST);
 	}
-	
-	
-	public static void getAfterSaleList(
-			Context context,
-			int recordType,
-			int customerId,
-			String search,
-			String q,
-			int page,
-			int rows,
+
+	public static void getAfterSaleList(Context context, int recordType,
+			int customerId, String search, String q, int page, int rows,
 			HttpCallback callback) {
 		String url = null;
 		switch (recordType) {
@@ -235,10 +241,7 @@ public class Config {
 		new HttpRequest(context, callback).post(url, params);
 	}
 
-	public static void cancelApply(
-			Context context,
-			int recordType,
-			int id,
+	public static void cancelApply(Context context, int recordType, int id,
 			HttpCallback callback) {
 		String url = null;
 		switch (recordType) {
@@ -257,11 +260,8 @@ public class Config {
 		new HttpRequest(context, callback).post(url, params);
 	}
 
-	public static void getAfterSaleDetail(
-			Context context,
-			int recordType,
-			int recordId,
-			HttpCallback callback) {
+	public static void getAfterSaleDetail(Context context, int recordType,
+			int recordId, HttpCallback callback) {
 		String url = null;
 		switch (recordType) {
 		case 0:
@@ -278,54 +278,42 @@ public class Config {
 		params.put("id", recordId);
 		new HttpRequest(context, callback).post(url, params);
 	}
-	public static void resubmitCancel(
-			Context context,
-			int recordId,
+
+	public static void resubmitCancel(Context context, int recordId,
 			HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("id", recordId);
 		new HttpRequest(context, callback).post(CANCELS_RESUBMITCANCEL, params);
 	}
-	public static void userGetUser(
-			Context context,
-			int customerId,
-			int page,
-			int rows,
-			HttpCallback callback) {
+
+	public static void userGetUser(Context context, int customerId, int page,
+			int rows, HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("customerId", customerId);
 		params.put("page", page);
 		params.put("rows", rows);
 		new HttpRequest(context, callback).post(USER_GETUSER, params);
 	}
-	public static void userDelectAgentUser(
-			Context context,
-			JSONArray customerArrayId,
-			int agentId,
-			HttpCallback callback) {
+
+	public static void userDelectAgentUser(Context context,
+			JSONArray customerArrayId, int agentId, HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("customerArrayId", customerArrayId);
 		params.put("agentId", agentId);
 		new HttpRequest(context, callback).post(USER_DELECTAGENTUSER, params);
 	}
-	public static void userGetTerminals(
-			Context context,
-			int customerId,
-			int page,
-			int rows,
-			HttpCallback callback) {
+
+	public static void userGetTerminals(Context context, int customerId,
+			int page, int rows, HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("customerId", customerId);
 		params.put("page", page);
 		params.put("rows", rows);
 		new HttpRequest(context, callback).post(USER_GETTERMINALS, params);
 	}
-	public static void agentsAddMark(
-			Context context,
-			int id,
-			String computer_name,
-			String track_number,
-			int customerId,
+
+	public static void agentsAddMark(Context context, int id,
+			String computer_name, String track_number, int customerId,
 			HttpCallback callback) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("id", id);
@@ -334,5 +322,12 @@ public class Config {
 		params.put("customerId", customerId);
 		new HttpRequest(context, callback).post(AGENTS_ADDMARK, params);
 	}
-	
+
+	public static void AddAdres1(Context context, String codeNumber,
+			HttpCallback callback) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("codeNumber", codeNumber);
+
+		new HttpRequest(context, callback).post(GETCODE4PHONE, params);
+	}
 }
